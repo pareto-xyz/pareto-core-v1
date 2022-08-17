9fe502cb0a8fad2934c63e90c701f71950e192e8
mhw32 <me@mikewuis.me>
2022-08-17 11:45:27 -0700

    Make upgradeable margin contract

--- contracts/ParetoV1Margin.sol
+++ contracts/ParetoV1Margin.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.9;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
 import "./libraries/Derivative.sol";
+import "./libraries/BlackScholesMath.sol";
 
 /**
  * @notice Contract acting as the margin account for a Pareto trader.
@@ -12,18 +18,21 @@ import "./libraries/Derivative.sol";
  * @dev The complete orderbook and matching are not performed within
  * this contract. Further, they are not performed on-chain. The owner
  * will post matched orders as positions. 
+ * @dev This contract is upgradeable with UUPS design. See
+ * https://blog.logrocket.com/using-uups-proxy-pattern-upgrade-smart-contracts/
  */
-contract ParetoV1Margin {
-
+contract ParetoV1Margin is
+    Initializable,
+    UUPSUpgradeable,
+    ReentrancyGuardUpgradeable,
+    OwnableUpgradeable
+{
     /************************************************
      * Constants and Immutables
      ***********************************************/
 
-    /// @notice Stores the address of the contract owner
-    address public immutable owner;
-
     /// @notice Stores the address for USDC
-    address public immutable usdc;
+    address public usdc;
 
     /// @notice Stores the amount of "cash" owned by users
     mapping(address => uint256) private balances;
@@ -38,24 +47,26 @@ contract ParetoV1Margin {
     uint256 private totalBalance;
 
     /************************************************
-     * Constructor and Modifers
+     * Initialization and Upgradeability
      ***********************************************/
 
     /**
      * @param usdc_ Address for the cash token (e.g. USDC)
      */
-    constructor(address usdc_) {
-        owner = msg.sender;
+    function initialize(address usdc_) public initializer {
         usdc = usdc_;
+
+        // Initialize the upgradeable dependencies
+        __ReentrancyGuard_init();
+        __Ownable_init();
     }
 
     /**
-     * @notice Modifier that restricts to only the owner
+     * @notice Safeguards against unauthored upgrades. UUPS requires 
+     * upgrade to be done from the logic contract
+     * @dev required by the OZ UUPS module
      */
-    modifier onlyOwner() {
-        require(msg.sender == owner, "onlyOwner: not the owner");
-        _;
-    }
+    function _authorizeUpgrade(address) internal override onlyOwner {}
 
     /************************************************
      * Events
@@ -83,7 +94,7 @@ contract ParetoV1Margin {
      * @dev Requires approval from `msg.sender`
      * @param amount Amount of USDC to deposit
      */
-    function deposit(uint256 amount) external {
+    function deposit(uint256 amount) external nonReentrant {
         require(amount > 0, "deposit: `amount` must be > 0");
 
         // Increment counters
@@ -105,13 +116,13 @@ contract ParetoV1Margin {
      * @dev Only successful if margin accounts remain satisfied post withdraw
      * @param amount Amount to withdraw
      */
-    function withdraw(uint256 amount) external {
+    function withdraw(uint256 amount) external nonReentrant {
     }
 
     /**
      * @notice Withdraw the maximum amount allowed
      */
-    function withdrawMax() external {
+    function withdrawMax() external nonReentrant {
     }
 
     /**
@@ -125,6 +136,7 @@ contract ParetoV1Margin {
      */
     function checkMargin(address user)
         external
+        nonReentrant
         returns (
             uint256 diff, 
             bool satisfied
@@ -180,6 +192,7 @@ contract ParetoV1Margin {
         address seller
     ) 
         external
+        nonReentrant
         onlyOwner 
     {
         require(bytes(bookId).length > 0, "recordOption: bookId is empty");
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@openzeppelin/hardhat-upgrades";
 
 dotenv.config();
 
