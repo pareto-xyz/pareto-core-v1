3a581433f034538ea080a7ab6d278bc04b7446dd
mhw32 <me@mikewuis.me>
2022-08-22 16:23:02 -0700

    Set out some of bsmath tests

--- test/libraries/blackscholesmath.test.ts
+++ test/libraries/blackscholesmath.test.ts
@@ -9,17 +9,54 @@ describe("BlackScholesMath Library", () => {
     const BlackScholesMathFactory = await hre.ethers.getContractFactory("TestBlackScholesMath");
     blackScholesMath = await BlackScholesMathFactory.deploy();
   });
+  describe("Computing probability factors", () => {
+    it("spot=1 ETH,strike=2 ETH,sigma=1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=2 ETH,sigma=0.1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=2 ETH,sigma=1,tau=1 day,rate=0", async () => {
+    });
+    it("spot=2 ETH,strike=2 ETH,sigma=1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=2 ETH,sigma=1,tau=1 week,rate=0.1 ETH", async () => {
+    });
+  });
   describe("Computing call price", () => {
-    
+    it("spot=1 ETH,strike=2 ETH,sigma=1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=2 ETH,sigma=0.1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=2 ETH,sigma=1,tau=1 day,rate=0", async () => {
+    });
+    it("spot=2 ETH,strike=2 ETH,sigma=1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=2 ETH,sigma=1,tau=1 week,rate=0.1 ETH", async () => {
+    });
   });
   describe("Computing put price", () => {
+    it("spot=2 ETH,strike=1 ETH,sigma=1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=2 ETH,strike=1 ETH,sigma=0.1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=2 ETH,strike=1 ETH,sigma=1,tau=1 day,rate=0", async () => {
+    });
+    it("spot=1 ETH,strike=1 ETH,sigma=1,tau=1 week,rate=0", async () => {
+    });
+    it("spot=2 ETH,strike=1 ETH,sigma=1,tau=1 week,rate=0.1 ETH", async () => {
+    });
   });
   describe("Converting vol to sigma", () => {
+    it("vol=0.1,tau=1 week", async () => {
+    });
+    it("vol=0.1,tau=1 day", async () => {
+    });
+    it("vol=1,tau=1 week", async () => {
+    });
   });
-  describe("Approximating vol from call price", () => {
-  });
-  describe("Approximating vol from put price", () => {
-  });
-  describe("Computing vega", () => {
-  });
+  // describe("Approximating vol from call price", () => {
+  // });
+  // describe("Approximating vol from put price", () => {
+  // });
+  // describe("Computing vega", () => {
+  // });
 });
