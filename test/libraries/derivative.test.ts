bdbfc43ad0b2cf51562d22f9dfea48770e91b0e0
mhw32 <me@mikewuis.me>
2022-08-31 11:06:56 -0700

    Update to derivatives

--- test/libraries/derivative.test.ts
+++ test/libraries/derivative.test.ts
@@ -661,38 +661,6 @@ describe("Derivative Library", () => {
       const hash2 = await derivative.hashOrder(order2);
       expect(hash1).to.be.equal(hash2);
     });
-    it("two different orders hash different", async () => {
-      const curTime = Math.floor(Date.now() / 1000);
-      const order1 = {
-        buyer: alice.address,
-        seller: bob.address,
-        tradePrice: ONE_ETH.mul(11).div(100),
-        quantity: 5,
-        option: {
-          optionType: 0,
-          strike: ONE_ETH.mul(11).div(10),
-          expiry: curTime + ONE_WEEK,
-          underlying: "0x0000000000000000000000000000000000000000",
-          decimals: 18,
-        }
-      };
-      const order2 = {
-        buyer: alice.address,
-        seller: bob.address,
-        tradePrice: ONE_ETH.mul(11).div(100),
-        quantity: 5,
-        option: {
-          optionType: 0,
-          strike: ONE_ETH.mul(11).div(10),
-          expiry: curTime + ONE_WEEK,
-          underlying: "0x0000000000000000000000000000000000000000",
-          decimals: 18,
-        }
-      };
-      const hash1 = await derivative.hashOrder(order1);
-      const hash2 = await derivative.hashOrder(order2);
-      expect(hash1).to.be.not.equal(hash2);
-    });
   });
 
   /****************************************
